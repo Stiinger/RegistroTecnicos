257fdfc4ac07eaf19ca274fe57e48e1789c01e66
Stiinger <JOSE_ORTEGA1@ucne.edu.do>
2025-01-18 17:26:51 -0400

    Creando TicketListScreen

---
+++ app/src/main/java/edu/ucne/registrotecnicos/presentation/tickets/TicketListScreen.kt
@@ -0,0 +1,118 @@
+package edu.ucne.registrotecnicos.presentation.tickets
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import edu.ucne.registrotecnicos.data.local.entities.TicketEntity
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TicketListScreen(
+    ticketList: List<TicketEntity>,
+    createTicket: () -> Unit,
+    goToMenu: () -> Unit,
+    goToTicket: (Int) -> Unit
+) {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            androidx.compose.material3.CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = "Lista de Tickets",
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                },
+                colors = androidx.compose.material3.TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimary
+                )
+            )
+            LazyColumn(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                item {
+                    TicketHeaderRow()
+                }
+                items(ticketList) { ticket ->
+                    TicketRow(
+                        ticket = ticket,
+                        goToTicket = { ticket.ticketId?.let { it1 -> goToTicket(it1) } }
+                    )
+                }
+            }
+        }
+        OutlinedButton(
+            onClick = { goToMenu() },
+            modifier = Modifier
+                .align(Alignment.BottomStart)
+                .padding(16.dp)
+        ) {
+            Text(text = "Menú Principal")
+        }
+        OutlinedButton(
+            onClick = { createTicket() },
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(16.dp)
+        ) {
+            Text(text = "Nuevo Ticket")
+        }
+    }
+}
+
+@Composable
+private fun TicketHeaderRow() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp, vertical = 4.dp)
+    ) {
+        Text(modifier = Modifier.weight(1f), text = "ID", style = MaterialTheme.typography.bodyLarge)
+        Text(modifier = Modifier.weight(1f), text = "Prioridad", style = MaterialTheme.typography.bodyLarge)
+        Text(modifier = Modifier.weight(1f), text = "Asunto", style = MaterialTheme.typography.bodyLarge)
+        Text(modifier = Modifier.weight(1f), text = "Cliente", style = MaterialTheme.typography.bodyLarge)
+    }
+    HorizontalDivider()
+}
+
+@Composable
+private fun TicketRow(
+    ticket: TicketEntity,
+    goToTicket: (Int) -> Unit
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable {
+                ticket.ticketId?.let { goToTicket(it)}
+            }
+            .padding(horizontal = 8.dp, vertical = 4.dp)
+    ) {
+        Text(modifier = Modifier.weight(1f), text = ticket.ticketId.toString())
+        Text(modifier = Modifier.weight(1f), text = ticket.prioridadId.toString())
+        Text(modifier = Modifier.weight(1f), text = ticket.asunto)
+        Text(modifier = Modifier.weight(1f), text = ticket.cliente)
+    }
+    HorizontalDivider()
+}
