3855bc9d456a69f0e17642dc79288376e1da48a0
Stiinger <JOSE_ORTEGA1@ucne.edu.do>
2025-01-18 17:25:36 -0400

    Agregando HomeScreen al Screen

--- app/src/main/java/edu/ucne/registrotecnicos/presentation/navigation/Screen.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/presentation/navigation/Screen.kt
@@ -7,4 +7,11 @@ sealed class Screen {
     data object TecnicoList : Screen() // Consulta
     @Serializable
     data class Tecnico(val tecnicoId: Int) : Screen() // Registro
+
+    @Serializable
+    data object TicketList : Screen() // Consulta
+    @Serializable
+    data class Ticket(val ticketId: Int) : Screen() // Registro
+    @Serializable
+    data object Home : Screen()
 }
