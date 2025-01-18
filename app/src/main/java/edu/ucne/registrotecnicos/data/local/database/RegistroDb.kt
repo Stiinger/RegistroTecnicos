8d0cad92a1f67fc613b1f4728a84d6b3b1998701
Stiinger <JOSE_ORTEGA1@ucne.edu.do>
2025-01-18 17:25:50 -0400

    Renombrar a RegistroDb

---
+++ app/src/main/java/edu/ucne/registrotecnicos/data/local/database/RegistroDb.kt
@@ -0,0 +1,24 @@
+package edu.ucne.registrotecnicos.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import edu.ucne.registrotecnicos.data.local.converters.DateConverter
+import edu.ucne.registrotecnicos.data.local.dao.TecnicoDao
+import edu.ucne.registrotecnicos.data.local.dao.TicketDao
+import edu.ucne.registrotecnicos.data.local.entities.TecnicoEntity
+import edu.ucne.registrotecnicos.data.local.entities.TicketEntity
+
+@Database(
+    entities = [
+        TecnicoEntity::class,
+        TicketEntity::class
+    ],
+    version = 3,
+    exportSchema = false
+)
+@TypeConverters(DateConverter::class)
+abstract class RegistroDb : RoomDatabase() {
+    abstract fun tecnicoDao(): TecnicoDao
+    abstract fun ticketDao(): TicketDao
+}
