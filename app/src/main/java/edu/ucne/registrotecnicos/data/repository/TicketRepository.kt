ccb3db2608dfced30e45241807fc87a988456c60
Stiinger <JOSE_ORTEGA1@ucne.edu.do>
2025-01-25 07:39:59 -0400

    Agregando funcionalidad para guardar y obtener mensajes de un ticket.
    - Guardar un nuevo mensaje asociado a un ticket.
    - Obtener todos los mensajes asociados a un ticket.

--- app/src/main/java/edu/ucne/registrotecnicos/data/repository/TicketRepository.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/data/repository/TicketRepository.kt
@@ -1,12 +1,15 @@
 package edu.ucne.registrotecnicos.data.repository
 
+import edu.ucne.registrotecnicos.data.local.dao.MensajeDao
 import edu.ucne.registrotecnicos.data.local.dao.TicketDao
+import edu.ucne.registrotecnicos.data.local.entities.MensajeEntity
 import edu.ucne.registrotecnicos.data.local.entities.TicketEntity
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class TicketRepository @Inject constructor(
-    private val ticketDao: TicketDao
+    private val ticketDao: TicketDao,
+    private val mensajeDao: MensajeDao
 ) {
     suspend fun save(ticket: TicketEntity) = ticketDao.save(ticket)
 
@@ -15,4 +18,13 @@ class TicketRepository @Inject constructor(
     fun getAll(): Flow<List<TicketEntity>> = ticketDao.getAll()
 
     suspend fun delete(ticket: TicketEntity) = ticketDao.delete(ticket)
+
+    suspend fun addMessageToTicket(ticketId: Int, contenido: String) {
+        val mensaje = MensajeEntity(ticketId = ticketId, contenido = contenido)
+        mensajeDao.save(mensaje)
+    }
+
+    fun getMessagesByTicketId(ticketId: Int): Flow<List<MensajeEntity>> {
+        return mensajeDao.getMessagesByTicketId(ticketId)
+    }
 }
