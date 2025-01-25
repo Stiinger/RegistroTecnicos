b11d53de51e0b795dd0b6283d0d508daf76a9d4a
Stiinger <JOSE_ORTEGA1@ucne.edu.do>
2025-01-25 07:44:53 -0400

    Creando ResponseViewModel

---
+++ app/src/main/java/edu/ucne/registrotecnicos/presentation/responses/ResponseViewModel.kt
@@ -0,0 +1,56 @@
+package edu.ucne.registrotecnicos.presentation.responses
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import edu.ucne.registrotecnicos.data.repository.TecnicoRepository
+import edu.ucne.registrotecnicos.data.repository.TicketRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Locale
+import javax.inject.Inject
+
+@HiltViewModel
+class ResponseViewModel @Inject constructor(
+    private val ticketRepository: TicketRepository,
+    private val tecnicoRepository: TecnicoRepository
+) : ViewModel() {
+    private val _uiState = MutableStateFlow(ResponseUiState())
+    val uiState = _uiState.asStateFlow()
+
+    fun loadResponses(ticketId: Int) {
+        viewModelScope.launch {
+            val ticket = ticketRepository.find(ticketId)
+            val tecnico = tecnicoRepository.find(ticket?.tecnicoId ?: 0)
+            val mensajes = ticketRepository.getMessagesByTicketId(ticketId).first()
+
+            val mensajesConDatos = mensajes.map { mensaje ->
+                val formattedDate = SimpleDateFormat("MM/dd/yyyy HH:mm", Locale.getDefault()).format(mensaje.fecha)
+                MensajeConDatos(
+                    contenido = mensaje.contenido,
+                    fechaHora = formattedDate,
+                    nombreTecnico = tecnico?.nombres ?: "Admin"
+                )
+            }
+            _uiState.update {
+                it.copy(
+                    ticketId = ticketId,
+                    nombreTecnico = tecnico?.nombres ?: "Admin",
+                    mensajes = mensajesConDatos
+                )
+            }
+        }
+    }
+
+    fun sendResponse(respuesta: String) {
+        viewModelScope.launch {
+            val mensaje = respuesta
+            ticketRepository.addMessageToTicket(uiState.value.ticketId, mensaje)
+            loadResponses(uiState.value.ticketId)
+        }
+    }
+}
