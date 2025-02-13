2cd9b291bf8214719da38bd05b419845cdeb7f72
Stiinger <JOSE_ORTEGA1@ucne.edu.do>
2025-02-13 09:26:33 -0400

    Creando PrioridadListScreen

---
+++ app/src/main/java/edu/ucne/registrotecnicos/presentation/prioridades/PrioridadListScreen.kt
@@ -0,0 +1,173 @@
+package edu.ucne.registrotecnicos.presentation.prioridades
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import edu.ucne.registrotecnicos.data.remote.dto.PrioridadDto
+import edu.ucne.registrotecnicos.presentation.components.TopBar
+
+@Composable
+fun PrioridadListScreen(
+    viewModel: PrioridadViewModel = hiltViewModel(),
+    createPrioridad: () -> Unit,
+    goToMenu: () -> Unit,
+    goToPrioridad: (Int) -> Unit
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    PrioridadListBodyScreen(
+        uiState,
+        createPrioridad,
+        goToMenu,
+        goToPrioridad
+    )
+}
+
+@Composable
+fun PrioridadListBodyScreen(
+    uiState: PrioridadUiState,
+    createPrioridad: () -> Unit,
+    goToMenu: () -> Unit,
+    goToPrioridad: (Int) -> Unit
+) {
+    Box(
+        modifier = Modifier.fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            TopBar(
+                titulo = "Lista de Prioridades",
+                onBackClick = { goToMenu() },
+                onCreateClick = { createPrioridad() }
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            if (uiState.isLoading) {
+                CircularProgressIndicator(
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+            }
+            uiState.errorMessage?.let { error ->
+                Text(
+                    text = error,
+                    color = MaterialTheme.colorScheme.error,
+                )
+            }
+            LazyColumn(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                item {
+                    PrioridadHeaderRow()
+                }
+                items(uiState.prioridades) {
+                    PrioridadRow(
+                        it,
+                        goToPrioridad
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun PrioridadHeaderRow() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .background(MaterialTheme.colorScheme.secondary)
+            .padding(vertical = 12.dp)
+    ) {
+        Text(
+            modifier = Modifier.weight(0.6f),
+            text = "ID",
+            style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+            color = MaterialTheme.colorScheme.onSecondary
+        )
+        Text(
+            modifier = Modifier.weight(1.8f),
+            text = "Descripción",
+            style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+            color = MaterialTheme.colorScheme.onSecondary
+        )
+        Text(
+            modifier = Modifier.weight(1.5f),
+            text = "DíasCompromiso",
+            style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+            color = MaterialTheme.colorScheme.onSecondary
+        )
+    }
+    HorizontalDivider(modifier = Modifier.padding(horizontal = 16.dp))
+}
+
+@Composable
+private fun PrioridadRow(
+    it: PrioridadDto,
+    goToPrioridad: (Int) -> Unit
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+            .clickable {
+                goToPrioridad(it.prioridadId)
+            },
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        shape = MaterialTheme.shapes.medium
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                modifier = Modifier.weight(0.5f),
+                text = it.prioridadId.toString(),
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+            Text(
+                modifier = Modifier.weight(2.5f),
+                text = it.descripcion,
+                style = MaterialTheme.typography.bodyMedium,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+            Text(
+                modifier = Modifier.weight(1.5f),
+                text = "${it.diasCompromiso}",
+                style = MaterialTheme.typography.bodyMedium,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+        }
+    }
+}
