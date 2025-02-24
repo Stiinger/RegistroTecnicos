eb8fdfff9ae886e72645021c456e201988d0fe79
Stiinger <JOSE_ORTEGA1@ucne.edu.do>
2025-02-24 12:58:07 -0400

    Modificando RegistroDb:
     1. Se actualizó la versión de la base de datos a 6.
     2. Se añadió la entidad PrioridadEntity.
     3. Se añadió el PrioridadDao.

--- app/src/main/java/edu/ucne/registrotecnicos/data/local/database/RegistroDb.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/data/local/database/RegistroDb.kt
@@ -5,9 +5,11 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import edu.ucne.registrotecnicos.data.local.converters.DateConverter
 import edu.ucne.registrotecnicos.data.local.dao.MensajeDao
+import edu.ucne.registrotecnicos.data.local.dao.PrioridadDao
 import edu.ucne.registrotecnicos.data.local.dao.TecnicoDao
 import edu.ucne.registrotecnicos.data.local.dao.TicketDao
 import edu.ucne.registrotecnicos.data.local.entities.MensajeEntity
+import edu.ucne.registrotecnicos.data.local.entities.PrioridadEntity
 import edu.ucne.registrotecnicos.data.local.entities.TecnicoEntity
 import edu.ucne.registrotecnicos.data.local.entities.TicketEntity
 
@@ -15,9 +17,10 @@ import edu.ucne.registrotecnicos.data.local.entities.TicketEntity
     entities = [
         TecnicoEntity::class,
         TicketEntity::class,
-        MensajeEntity::class
+        MensajeEntity::class,
+        PrioridadEntity::class
     ],
-    version = 5,
+    version = 6,
     exportSchema = false
 )
 @TypeConverters(DateConverter::class)
@@ -25,4 +28,5 @@ abstract class RegistroDb : RoomDatabase() {
     abstract fun tecnicoDao(): TecnicoDao
     abstract fun ticketDao(): TicketDao
     abstract fun mensajeDao(): MensajeDao
+    abstract fun prioridadDao(): PrioridadDao
 }
